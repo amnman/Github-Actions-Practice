32279435bdfe94226db0a20920062858c8e41ee7
maneeshmurali <maneesh.murali@gmail.com>
2025-12-06 18:00:31 +0530

    Test Failure simulation

--- src/main/java/com/github/actions/gha/GhaApplication.java
+++ src/main/java/com/github/actions/gha/GhaApplication.java
@@ -3,7 +3,7 @@ package com.github.actions.gha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplications
 public class GhaApplication {
 
 	public static void main(String[] args) {
