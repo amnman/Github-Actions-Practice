80d5c43881d02b93515c41ae5d46f904397aaf45
maneeshmurali <maneesh.murali@gmail.com>
2025-12-12 09:26:12 +0530

    Test GHA workflow

--- src/main/java/com/github/actions/gha/GhaApplication.java
+++ src/main/java/com/github/actions/gha/GhaApplication.java
@@ -3,6 +3,7 @@ package com.github.actions.gha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Spring Boot Application
 @SpringBootApplication
 public class GhaApplication {
 
