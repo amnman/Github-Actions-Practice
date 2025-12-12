466caa54d3368f614b902ea4f0216ffbe770677c
maneeshmurali <maneesh.murali@gmail.com>
2025-12-12 09:34:17 +0530

    Test GHA workflow trigger on PR closure

--- src/test/java/com/github/actions/gha/GhaApplicationTests.java
+++ src/test/java/com/github/actions/gha/GhaApplicationTests.java
@@ -3,6 +3,7 @@ package com.github.actions.gha;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//Test GHA flow
 @SpringBootTest
 class GhaApplicationTests {
 
