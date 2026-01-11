c6923643097dc02a35537666f2612532e6583c26
maneeshmurali <maneesh.murali@gmail.com>
2026-01-11 06:17:47 +0530

    Javascript Action Testing

---
+++ .github/actions/javascript-action/main.js
@@ -0,0 +1,9 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as exec from "@actions/exec";
+
+run=()=> {
+    core.info("JavaScript Action Triggered!!");
+}
+
+run();
